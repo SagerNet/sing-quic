0c50662e02955c2d6ccb02af80c91c24255ced74
世界 <i@sekai.icu>
2024-04-29 11:44:31 +0800

    Fix RoundTripper usage

--- quic.go
+++ quic.go
@@ -15,7 +15,7 @@ import (
 type Config interface {
 	Dial(ctx context.Context, conn net.PacketConn, addr net.Addr, config *quic.Config) (quic.Connection, error)
 	DialEarly(ctx context.Context, conn net.PacketConn, addr net.Addr, config *quic.Config) (quic.EarlyConnection, error)
-	CreateTransport(conn net.PacketConn, quicConnPtr *quic.EarlyConnection, serverAddr M.Socksaddr, quicConfig *quic.Config, enableDatagrams bool) http.RoundTripper
+	CreateTransport(conn net.PacketConn, quicConnPtr *quic.EarlyConnection, serverAddr M.Socksaddr, quicConfig *quic.Config) http.RoundTripper
 }
 
 type ServerConfig interface {
@@ -58,9 +58,9 @@ func DialEarly(ctx context.Context, conn net.PacketConn, addr net.Addr, config a
 	return quic.DialEarly(ctx, conn, addr, tlsConfig, quicConfig)
 }
 
-func CreateTransport(conn net.PacketConn, quicConnPtr *quic.EarlyConnection, serverAddr M.Socksaddr, config aTLS.Config, quicConfig *quic.Config, enableDatagrams bool) (http.RoundTripper, error) {
+func CreateTransport(conn net.PacketConn, quicConnPtr *quic.EarlyConnection, serverAddr M.Socksaddr, config aTLS.Config, quicConfig *quic.Config) (http.RoundTripper, error) {
 	if quicTLSConfig, isQUICConfig := config.(Config); isQUICConfig {
-		return quicTLSConfig.CreateTransport(conn, quicConnPtr, serverAddr, quicConfig, enableDatagrams), nil
+		return quicTLSConfig.CreateTransport(conn, quicConnPtr, serverAddr, quicConfig), nil
 	}
 	tlsConfig, err := config.Config()
 	if err != nil {
@@ -69,7 +69,6 @@ func CreateTransport(conn net.PacketConn, quicConnPtr *quic.EarlyConnection, ser
 	return &http3.RoundTripper{
 		TLSClientConfig: tlsConfig,
 		QUICConfig:      quicConfig,
-		EnableDatagrams: enableDatagrams,
 		Dial: func(ctx context.Context, addr string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlyConnection, error) {
 			quicConn, err := quic.DialEarly(ctx, conn, serverAddr.UDPAddr(), tlsCfg, cfg)
 			if err != nil {
