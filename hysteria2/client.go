8d62e983d57b527938553e52a1ff445282eea117
wwqgtxx <wwqgtxx@gmail.com>
2025-04-04 11:19:27 +0800

    Fix close response body

--- hysteria2/client.go
+++ hysteria2/client.go
@@ -173,6 +173,7 @@ func (c *Client) offerNew(ctx context.Context) (*clientQUICConnection, error) {
 		packetConn.Close()
 		return nil, err
 	}
+	response.Body.Close()
 	if response.StatusCode != protocol.StatusAuthOK {
 		if quicConn != nil {
 			quicConn.CloseWithError(0, "")
@@ -180,7 +181,6 @@ func (c *Client) offerNew(ctx context.Context) (*clientQUICConnection, error) {
 		packetConn.Close()
 		return nil, E.New("authentication failed, status code: ", response.StatusCode)
 	}
-	response.Body.Close()
 	authResponse := protocol.AuthResponseFromHeader(response.Header)
 	actualTx := authResponse.Rx
 	if actualTx == 0 || actualTx > c.sendBPS {
